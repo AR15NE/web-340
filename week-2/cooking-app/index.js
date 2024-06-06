c24226fa14c1a9352599409d0764d78b735309d1
Amanda Rovey <155773970+AR15NE@users.noreply.github.com>
2024-06-06 07:49:01 -0500

    Week 2
    All changes for the week 2 assignment: 2.2 - Closures

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,14 @@
-/**
- * Author:
- * Date:
- * File Name:
- * Description:
+/*
+ * Author: Amanda Rovey
+ * Date: June 5th 2024
+ * File Name: recipes.js 
+ * Description: js file for assignment 2.2
 */
 
-// TODO: Import your module using require
+// Import your module using require - this will access the functions in the recipes.js file 
+const recipes = require('./recipes');
 
-// TODO: Implement your CLI program here
+// Implement your CLI program here
+console.log(recipes.createRecipe(['flour', 'sugar', 'eggs']));
+console.log(recipes.setTimer(5));
+console.log(recipes.quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,34 @@
-/**
- * Author:
- * Date:
- * File Name:
- * Description:
+/*
+ * Author: Amanda Rovey
+ * Date: June 5th 2024
+ * File Name: recipes.js 
+ * Description: js file for assignment 2.2
 */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  // This joins the ingredients together into a string separated by commas and a space
+  const ingredientList = ingredients.join(', ');
+  // Creates the recipe string with the correct formatting
+  const recipeString = `Recipe created with ingredients: ${ingredientList}`;
+  return recipeString;
 }
 
-// Define the setTimer function
+// Define the setTimer function - Sets a timer for a specific number of minutes and returns a confirmation message
 function setTimer(minutes) {
-  // TODO: Implement this function
+  // Ensures the output string matches the expected format exactly
+  return `Timer set for ${minutes} minutes`; //The confirmation message for the user 
 }
 
-// Define the quit function
+// Define the quit function - Exits the program and gives a confirmation message to the user. 
 function quit() {
-  // TODO: Implement this function
+  // Returns the expected string
+  return 'Program exited';
 }
 
-// TODO: Export the functions
+// Export the functions
+module.exports = {
+  createRecipe,
+  setTimer,
+  quit
+};
